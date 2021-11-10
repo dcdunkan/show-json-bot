1403446ee9cc72a73b424ae93de0074a212f32d9
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2021-11-10 12:48:12 +0530

    Part of the previous fix

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,32 @@ import env from "./env";
 // grammy.dev
 import { Bot, GrammyError, HttpError } from "grammy";
 const bot = new Bot(env.BOT_TOKEN);
-db.init();
+
+if (process.env.DB_ENABLE === 'true') {
+  db.init();
+}
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  let error_message = `Error while handling update ${ctx.update.update_id}:\n`;
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    error_message += "Error in request:" + e.description;
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    error_message += `Could not contact Telegram: ${JSON.stringify(e)}`;
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+  if (env.CHAT_LOG === true) {
+    let msg = `<a href="t.me/${bot.botInfo.username}">JSON Bot</a> error log:\n${error_message}\n#show_json_bot @${bot.botInfo.username}.`;
+    bot.api.sendMessage(process.env.CHAT_ID, msg, {
+      parse_mode: "HTML",
+      disable_web_page_preview: true,
+    });
+  }
+});
 
 // middleware to manage any incoming message updates and return starter data structure.
 import updateHandler from "./middlewares/updateHandler";
@@ -33,27 +58,5 @@ bot.command("start", startHandler);
 bot.command("help", helpHandler);
 
 bot.command(["me", "rate"], meHandler);
-
-bot.catch((err) => {
-  const ctx = err.ctx;
-  let error_message = `Error while handling update ${ctx.update.update_id}:\n`;
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    error_message += "Error in request:" + e.description;
-    console.error("Error in request:", e.description);
-  } else if (e instanceof HttpError) {
-    error_message += `Could not contact Telegram: ${JSON.stringify(e)}`;
-    console.error("Could not contact Telegram:", e);
-  } else {
-    console.error("Unknown error:", e);
-  }
-  if (env.CHAT_LOG === true) {
-    let msg = `<a href="t.me/${bot.botInfo.username}">JSON Bot</a> error log:\n${error_message}\n#show_json_bot @${bot.botInfo.username}.`;
-    bot.api.sendMessage(process.env.CHAT_ID, msg, {
-      parse_mode: "HTML",
-      disable_web_page_preview: true,
-    });
-  }
-});
 
 export default bot;
